4692e3a5a891df20f7fce0909c4c8bd521c223fb
Alex Developer <ak@onlym.ru>
2022-05-05 22:08:40 +0500

    Add resizing

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 import imagemin from 'imagemin';
 import imageminMozjpeg from 'imagemin-mozjpeg';
 import imageminPngquant from 'imagemin-pngquant';
+import sizeOf from 'image-size';
+import * as fs from 'fs';
+import * as path from 'path';
+import imageThumbnail from 'image-thumbnail';
+
+const inputFilePath = 'input';
+const outputFilePath = 'output';
 
 (async () => {
-	await imagemin(['input/*.jpg', 'input/*.png'], {
-		destination: 'output',
+	let files = await fs.readdirSync(inputFilePath);
+
+	await files.forEach(async file => {
+		let extname = path.extname(file).toLowerCase();
+		if (extname == '.jpg' || extname == '.png') {
+			let imagePath = inputFilePath + '/' + file;
+			let dimensions = sizeOf(inputFilePath + '/' + file);
+			let imageWidth = dimensions.width;
+			let imageHeight = dimensions.height;
+
+			if (imageWidth > imageHeight && imageWidth > 1200) {
+				let imageMini = await imageThumbnail(imagePath, {
+					width: 1200
+				})
+				fs.writeFileSync(imagePath, imageMini);
+			}
+			else if (imageHeight > imageWidth && imageHeight > 1200) {
+				let imageMini = await imageThumbnail(imagePath, {
+					height: 1200
+				})
+				fs.writeFileSync(imagePath, imageMini);
+			}
+		}
+	});
+
+	await imagemin([inputFilePath + '/*.{jpg,png,JPG,PNG}'], {
+		destination: outputFilePath,
 		plugins: [
 			imageminMozjpeg({
-                quality: 70
+                quality: 80
             }),
             imageminPngquant()
 		],
