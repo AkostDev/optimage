c8db9c32bb4efd663034472db2ee57ad934808d7
Alex Developer <ak@onlym.ru>
2022-05-06 16:21:32 +0500

    Bugfix and optimizations

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import imagemin from 'imagemin';
-import imageminMozjpeg from 'imagemin-mozjpeg';
-import imageminPngquant from 'imagemin-pngquant';
 import sizeOf from 'image-size';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -10,40 +7,54 @@ const inputFilePath = 'input';
 const outputFilePath = 'output';
 
 (async () => {
-	let files = await fs.readdirSync(inputFilePath);
+	let files = fs.readdirSync(inputFilePath)
 
-	await files.forEach(async file => {
+	files.forEach(async (file) => {
 		let extname = path.extname(file).toLowerCase();
 		if (extname == '.jpg' || extname == '.png') {
-			let imagePath = inputFilePath + '/' + file;
-			let dimensions = sizeOf(inputFilePath + '/' + file);
+			let imagePathInput = inputFilePath + '/' + file;
+			let imagePathOutput = outputFilePath + '/' + file;
+
+			let dimensions = sizeOf(imagePathInput);
 			let imageWidth = dimensions.width;
 			let imageHeight = dimensions.height;
 
 			if (imageWidth > imageHeight && imageWidth > 1200) {
-				let imageMini = await imageThumbnail(imagePath, {
-					width: 1200
+				let imageMini = await imageThumbnail(imagePathInput, {
+					width: 1200,
+					jpegOptions: {
+						force: true,
+						quality: 80
+					}
 				})
-				await fs.writeFileSync(imagePath, imageMini);
+
+				fs.writeFileSync(imagePathOutput, imageMini);
 			}
 			else if (imageHeight > imageWidth && imageHeight > 1200) {
-				let imageMini = await imageThumbnail(imagePath, {
-					height: 1200
+				let imageMini = await imageThumbnail(imagePathInput, {
+					height: 1200,
+					jpegOptions: {
+						force: true,
+						quality: 80
+					}
 				})
-				await fs.writeFileSync(imagePath, imageMini);
+
+				fs.writeFileSync(imagePathOutput, imageMini);
+			}
+			else {
+				let imageMini = await imageThumbnail(imagePathInput, {
+					width: imageWidth,
+					height: imageHeight,
+					jpegOptions: {
+						force: true,
+						quality: 80
+					}
+				})
+
+				fs.writeFileSync(imagePathOutput, imageMini);
 			}
 		}
-	});
-
-	await imagemin([inputFilePath + '/*.{jpg,png,JPG,PNG}'], {
-		destination: outputFilePath,
-		plugins: [
-			imageminMozjpeg({
-                quality: 80
-            }),
-            imageminPngquant()
-		],
-	});
+	})
 
 	console.log('Images optimized');
 })();
